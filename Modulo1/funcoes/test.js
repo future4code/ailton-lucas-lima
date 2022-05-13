4206c377b8a1526862741d96453ba36780c69077
Lucas vinicius vieira <lucaslima0807@HOTMAIL.COM>
2022-05-13 21:20:49 +0200

    Nao entendo os exercicico

---
+++ Modulo1/funcoes/test.js
@@ -0,0 +1,58 @@
+// function soma (numero1, numero2) {
+
+// return numero1 + numero2
+
+    
+// }
+
+// let numero1= (2)
+// let numero2 = (10)
+
+// const resultado = soma (numero1, numero2)
+// console.log (resultado)
+// ..............................................................
+// let textoDoUsuario = prompt("Insira um texto");
+
+// function outraFuncao (texto, texto2) {
+// 	return texto.toLowerCase().includes("cenoura")
+// }
+
+// const resposta = outraFuncao(textoDoUsuario)
+// console.log(resposta)
+
+// ESCRITA DE CÓDIGO
+1
+//a)
+// const nome = 'Amanda'
+// const idade = 33
+// const cidade = 'Poços de Caldas'
+// const estudante = 'não sou estudante'
+
+
+// function imprimeMensagem() {
+// 	console.log(`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e ${estudante}`)
+// }
+
+// imprimeMensagem()
+const nome = prompt ("cual es tu nombre ?")
+const idade = prompt ("cuantos años tienes ?")
+const cidade = prompt ("En cual ciudad vives ?")
+const estudante = prompt ("Cual es tu profesion ?")
+
+// const estudante1 = (estudante)
+// const nome1 = (nome)
+// const idade1 = (idade)
+// const cidade1= (cidade)
+
+
+function EmtregarTexto() {
+   console.log (`Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade} e ${estudante}`)
+
+
+    
+}
+
+
+
+
+EmtregarTexto ()
