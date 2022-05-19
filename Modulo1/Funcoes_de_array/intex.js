3a4dfb1efa889b584568b19ad49155a796b6adde
Lucas vinicius vieira <lucaslima0807@HOTMAIL.COM>
2022-05-19 23:36:51 +0200

    funcoes_arrays

---
+++ Modulo1/Funcoes_de_array/intex.js
@@ -0,0 +1,34 @@
+// const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" }
+//   ]
+  
+//   const novoArrayA = usuarios.map((item, index, array) => {
+//      console.log(item, index, array)
+//   })
+  
+
+//   const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" },
+//   ]
+  
+//   const novoArrayB = usuarios.map((item, index, array) => {
+//      return item.nome
+//   })
+  
+//   console.log(novoArrayB)
+
+//   const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" },
+//   ]
+  
+//   const novoArrayC = usuarios.filter((item, index, array) => {
+//      return item.apelido !== "Chijo"
+//   })
+  
+//   console.log(novoArrayC)
